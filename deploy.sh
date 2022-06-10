0e2e1faad1ed7ff83cd34dc1c948f0e39e4be8a4
Eslam Abdo <eslamabdo@192.168.1.7>
2022-06-10 03:35:06 +0200

    push all

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,10 @@ sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 echo "Done installing docker"
 cd backend 
 docker build -f Dockerfile -t backend .
