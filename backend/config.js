eecfa92f8787347776690e76f85fba6c679ce7fc
Eslam Abdo <eslamabdo@192.168.1.7>
2022-06-10 04:35:53 +0200

    push all

--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  CORS_ORIGIN: [
-    "http://localhost:3000",
-    "http://0.0.0.0:3000",
-    "http://18.118.147.63:3000",
-    "http://18.118.147.63:8080",
-  ],
+  CORS_ORIGIN: "*",
 };
